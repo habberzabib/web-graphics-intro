bcebae72fe117fd3325416b22bb3a81d6c8deb69
habberzabib <145040111+habberzabib@users.noreply.github.com>
2023-11-22 11:56:57 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,26 +4,28 @@ let ctx = cnv.getContext("2d");
 cnv.width = 600;
 cnv.height = 400;
 
+//Use any valid css colours: name, rgb(), rgba(), hex #FF0033
+
 //DRAW FILLED IN RECTANGLE
-ctx.fillStyle = "blue";
+ctx.fillStyle = "orange";
 ctx.fillRect(50, 50, 100, 60);
 // x, y, w, h
 // (x,y) = top-left corner of the rectangle
 
 //OUTLINED RECT
-ctx.strokeStyle = "blue";
+ctx.strokeStyle = "orange";
 ctx.lineWidth = 4;
 ctx.strokeRect(50, 150, 100, 60);
 
 //SOLID TEXT
 ctx.font = "30px Arial";
-ctx.fillStyle = "green";
+ctx.fillStyle = "black";
 ctx.fillText("Canvas Text", 250, 100);
 
 //OUTLINED TEXT
 ctx.font = "36px Times";
 ctx.lineWidth = 1;
-ctx.strokeStyle = "green";
+ctx.strokeStyle = "black";
 ctx.strokeText("Canvas Text", 250, 200);
 
 //LINE
@@ -39,33 +41,59 @@ ctx.fillStyle = "#00FF00";
 ctx.beginPath();
 ctx.moveTo(250, 250);
 ctx.lineTo(300, 250);
-ctx.lineTo(275, 300);
+ctx.lineTo(250, 300);
 ctx.fill();
 
 //OUTLINED TRIANGLE
 ctx.lineWidth = 2;
 ctx.strokeStyle = "green";
 ctx.beginPath();
-ctx.moveTo(350, 250);
-ctx.lineTo(400, 250);
-ctx.lineTo(375, 300);
+ctx.moveTo(325, 250);
+ctx.lineTo(375, 250);
+ctx.lineTo(350, 300);
 ctx.closePath();
 ctx.stroke();
 //CAN BE APPLIED TO ANY N-SIDED POLYGON WITH MORE ctx.lineTo()
 
-//SOLID CIRCLE
-ctx.fillStyle = "blue";
+//OUTLINED PENTAGON
+ctx.lineWidth = 2;
+ctx.strokeStyle = "red";
 ctx.beginPath();
-ctx.arc(275, 350, 25, 0, 2 * Math.PI);
+ctx.moveTo(375, 225);
+ctx.lineTo(425, 200);
+ctx.lineTo(450, 250);
+ctx.lineTo(400, 250);
+ctx.lineTo(350, 200);
+
+ctx.closePath();
+ctx.stroke();
+
+//SOLID CIRCLE
+ctx.fillStyle = "purple";
+ctx.beginPath();
+ctx.arc(250, 350, 25, 0, 2 * Math.PI);
 // x, y, r, start angle (in radians) dip, end angle
 //(x, y) = center of the circle
 ctx.fill();
 
 //OUTLINED CIRCLE
 ctx.lineWidth = 3;
-ctx.strokeStyle = "blue";
+ctx.strokeStyle = "purple";
 ctx.beginPath();
-ctx.arc(375, 350, 25, 0, 2 * Math.PI);
+ctx.arc(350, 350, 25, 0, 2 * Math.PI);
+ctx.stroke();
+
+//HALF CIRCLE
+ctx.fillStyle = "purple";
+ctx.beginPath();
+ctx.arc(450, 350, 25, 0, Math.PI);
+ctx.fill();
+
+//OUTLINED HALF CIRCLE
+ctx.lineWidth = 3;
+ctx.strokeStyle = "purple";
+ctx.beginPath();
+ctx.arc(550, 350, 25, 0, Math.PI);
 ctx.stroke();
 
 //DRAW AN IMAGE
